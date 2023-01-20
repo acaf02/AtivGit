8a436019a87e898c07eeb9804c62235fb356d1a8
Ana Carolina <anacarol.alves1@outlook.com>
2023-01-20 11:04:04 -0300

    adição de arquivo java

---
+++ Git.java
@@ -0,0 +1,19 @@
+
+package git;
+
+public class Git {
+    static int tabuada (int numero){
+        int mult =0;
+        for (int i = 1; i <= 10; i++) {
+            mult = numero * i;
+            
+            System.out.println(numero + " * " + i + " = " + mult);
+        }
+        return mult;
+    }
+    public static void main(String[] args) {
+        int num = 5;
+        tabuada(num);
+    }
+    
+}
